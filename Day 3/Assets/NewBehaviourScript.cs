1f708c723fb5153fc7774fa46efc93cdd1d34c7c
yousef-Alhouti <tophatgamingALT@gmail.com>
2022-08-05 23:52:34 +0300

    Day 3

--- Day 3/Assets/NewBehaviourScript.cs
+++ Day 3/Assets/NewBehaviourScript.cs
@@ -10,10 +10,14 @@ public class NewBehaviourScript : MonoBehaviour
     string VillianName = "Hamad";
     int VillainPower = 10;
 
+   
 
  // Start is called before the first frame update
     void Start()
     {
+
+        print("                     ");
+
         if (heropower > VillainPower)
         {
             print("The hero has more power!");
@@ -28,13 +32,56 @@ public class NewBehaviourScript : MonoBehaviour
         {
             print("The villian has more power than the hero!");
         }
+        print("                     ");
+        {
+            float PlayerSpeed = 0.0F;
+            float SetSpeed = 0.0F;
+            float Speed = 2.5F;
 
+            SetSpeed = Speed;
+            PlayerSpeed = Speed;
+           
+
+            print(SetSpeed);
+            print(Speed);
+            print(PlayerSpeed);
+
+        }
+        print("                     ");
+        {
+            
+            float NewSpeed = 10F;
+            float OldSpeed = 10F;
+
+            if (NewSpeed > OldSpeed)
+            {
+                print("The highest speed is");
+                print(NewSpeed);
+            }
+
+            else if (NewSpeed == OldSpeed)
+            {
+                print("The highest speed is");
+                print(NewSpeed);
+                print("and");
+                print(OldSpeed);
+            }
+
+            else
+            {
+                print("The highest speed is");
+                print( OldSpeed);
+            }
+
+        }
+    
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
     
